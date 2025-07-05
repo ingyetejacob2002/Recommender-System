0eecc8016db8dd9d5b8015f27c57f86099332e6e
ingyetejacob2002 <ingyetejacob2002@gmail.com>
2025-07-05 07:40:02 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+model=joblib.load("C://Users/user/Desktop/streamlit Applications/model.joblib")
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get form data
+        age = int(request.form['age'])
+        gender = int(request.form['gender'])  # Assume gender is encoded as 0/1
+        
+        # Make prediction
+        input_features = np.array([[age, gender]])
+        prediction = model.predict(input_features)
+        
+        return render_template('index.html', prediction_text=f'Predicted Genre: {prediction[0]}')
+    except Exception as e:
+        return render_template('index.html', prediction_text=f'Error: {str(e)}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
